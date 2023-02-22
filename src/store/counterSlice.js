c4f919e32d2ded799686e20e1f7f7306a972f510
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-02-22 10:14:22 +0200

    Add CounterSlice

--- src/store/counterSlice.js
+++ src/store/counterSlice.js
@@ -4,19 +4,22 @@ const counterSlice = createSlice({
   name: "counter",
   initialState: {
     count: 0,
+    step: 1
   },
   reducers: {
     //methods
     increment(state, action) {
-      console.log("state", state);
-      console.log("action", action);
+      state.count+=state.step;
     },
     decrement(state, action) {
-      console.log("state", state);
-      console.log("action", action);
+      state.count-=state.step;
     },
+    setStep(state, action){
+      const {value} = action.payload;
+      state.step = Number(value);
+    }
   },
 });
 
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrement, setStep } = counterSlice.actions;
 export default counterSlice.reducer;
