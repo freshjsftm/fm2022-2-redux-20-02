ef0d80ec24cb35e4942a29afb83a9974517f8443
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-02-22 11:40:52 +0200

    Add ready Todo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Counter from "./components/Counter";
+import Todo from "./components/Todo";
 
 function App() {
   return (
     <>
       <Counter />
+      <hr /><hr /><hr />
+      <Todo />
     </>
   );
 }
