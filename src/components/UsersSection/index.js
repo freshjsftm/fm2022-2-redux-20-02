511b481afbaab0320244a1971508fe0ab08693f2
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-04-03 11:48:31 +0300

    Add example connect

--- src/components/UsersSection/index.js
+++ src/components/UsersSection/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useEffect} from "react";
+import { connect } from "react-redux";
+import { getAllUsers } from "../../store/usersSlice";
 
-const UsersSection = () => {
+const UsersSection = (props) => {
+  const { users, error, isFetching, loadedUsers } = props;
+  useEffect(()=>{
+    loadedUsers()
+  }, [loadedUsers])
   return (
     <section>
       <h2>Users List</h2>
+      {error && <h3>Error!</h3>}
+      {isFetching && <h3>Loading...</h3>}
+      {users.map((user, i) => (
+        <article key={i}>{JSON.stringify(user)}</article>
+      ))}
     </section>
   );
-}
+};
 
-export default UsersSection;
+const mapStateToProps = (state) => state.users; //initialState || state from usersSlice
+const mapDispatchToProps = (dispatch) => ({
+  loadedUsers: (params)=>dispatch(getAllUsers(params))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(UsersSection);
--- src/store/usersSlice.js
+++ src/store/usersSlice.js
@@ -1,4 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const getAllUsers = createAsyncThunk(
+  "users/getAllUsers",
+  async (params={}, thunkAPI) => {
+    try {
+      const { dispatch } = thunkAPI;
+      const data = await fetch("https://randomuser.me/api/?results=3").then((res) =>
+        res.json()
+      );
+      //return data;
+      dispatch(loadUsers(data.results));
+    } catch (error) {}
+  }
+);
 
 const usersSlice = createSlice({
   name: "users",
@@ -7,7 +21,12 @@ const usersSlice = createSlice({
     error: null,
     isFetching: false,
   },
-  reducers: {},
+  reducers: {
+    loadUsers(state, action) {
+      state.users = action.payload;
+    },
+  },
 });
 
+const { loadUsers } = usersSlice.actions;
 export default usersSlice.reducer;
